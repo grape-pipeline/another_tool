32d3d853c79728680634b4caf670b358e439f4a6
Thasso Griebel <thasso.griebel@gmail.com>
2013-04-24 15:13:35 +0200

    Updated version and setup cleanup

--- another/__init__.py
+++ another/__init__.py
@@ -6,3 +6,4 @@ Another tool library
 The library consists of classes and methods to define computational tools
 that can be executed remotely and can be chained to pipelines of tools.
 """
+__version__ = "1.0"
