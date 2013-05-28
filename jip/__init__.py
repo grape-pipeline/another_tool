c20833a8e81e88efb88a0634a58722d6128a5f5a
Thasso Griebel <thasso.griebel@gmail.com>
2013-05-28 12:27:38 +0200

    added grape tools command and cluster.json and jobs.json to bundle

--- jip/__init__.py
+++ jip/__init__.py
@@ -1,2 +1,13 @@
 __author__ = 'thasso'
 __version__ = "1.0"
+
+
+def discover():
+    """Search loaded modules for tool implementation
+    and returns a list of classes that are subclasses of
+    :class:jip.tools.Tool
+
+    :returns tools: list of Tool classes
+    """
+    from . import tools
+    return tools.Tool.__subclasses__()
